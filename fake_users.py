f4599a8d7c04a71ff8c9f94f9060d516e0ba3505
Xavier Puentes <xavier@Xaviers-MacBook-Pro.local>
2019-06-01 16:00:28 -0500

    Added premium users to faker user script

--- fake_users.py
+++ fake_users.py
@@ -6,18 +6,33 @@ django.setup()
 
 import random
 
-from api.models import User
+from api.models import User, Subtopic, Discussion, Comments
 from faker import Faker
 
 fake = Faker()
 
-def create_users(N):
+
+def create_non_premium_users(N):
     for _ in range(N):
         newuser = User.objects.get_or_create(
-        uuid = fake.uuid4,
-        username = fake.user_name,
-        email = fake.email,
-        password = fake.password(length=10, special_chars=True, digits=True, upper_case=True, lower_case=True))
+            uuid = fake.uuid4,
+            username = fake.user_name,
+            email = fake.email,
+            password = fake.password(length=10, special_chars=True, digits=True, upper_case=True, lower_case=True)
+        )
 
-create_users(400)
+
+def create_premium_users(N):
+    for _ in range(N):
+        newuser = User.objects.get_or_create(
+            uuid = fake.uuid4,
+            username = fake.user_name,
+            email = fake.email,
+            password = fake.password(length=10, special_chars=True, digits=True, upper_case=True, lower_case=True),
+            premium = True
+        )
+
+
+create_non_premium_users(250)
+create_premium_users(125)
 print("Users created...")
