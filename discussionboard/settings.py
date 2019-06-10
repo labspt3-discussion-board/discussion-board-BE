821b71b3a0ec9e4d7df8a931261221dbbc7fd2a3
Brandon <brandon.g.elzy@gmail.com>
2019-06-10 08:02:15 -0500

    Brandon G. Elzy - Created Facebook OAuth endpoint

--- discussionboard/settings.py
+++ discussionboard/settings.py
@@ -48,7 +48,7 @@ CORS_ALLOW_HEADERS = list(default_headers) + [
 
 CORS_ALLOW_CREDENTIALS = True
 SESSION_COOKIE_SAMESITE = None
-# SESSION_COOKIE_DOMAIN = 'heroku.com'
+SESSION_COOKIE_DOMAIN = 'heroku.com'
 SESSION_COOKIE_HTTPONLY = False
 
 # Rest Framework global settings
