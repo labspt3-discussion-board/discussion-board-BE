5f16ad923035ddcf34ef753bfc4cdcedc7cf9d7e
Brandon <brandon.g.elzy@gmail.com>
2019-06-11 06:59:32 -0500

    Brandon G. Elzy - Changed subtopic to subforum

--- api/serializers.py
+++ api/serializers.py
@@ -1,22 +1,22 @@
 from rest_framework             import serializers
 # from django.contrib.auth.models import User
-from api.models                 import Subtopic, Discussion, Comments
+from api.models                 import Subforum, Discussion, Comments
 from django.conf                import settings
 from django.contrib.auth        import get_user_model
 from django.db.models           import Count
 
 
-class SubtopicSerializer(serializers.ModelSerializer):
+class SubforumSerializer(serializers.ModelSerializer):
     class Meta:
-        model = Subtopic
+        model = Subforum
         fields = ('id', 'name', 'private', 'created_at', 'owner')
 
 class UserSerializer(serializers.ModelSerializer):
-    subtopics = SubtopicSerializer(many=True, read_only=True, default=[])
+    subforums = SubforumSerializer(many=True, read_only=True, default=[])
 
     class Meta:
         model = get_user_model()
-        fields = ('id', 'username', 'email', 'first_name', 'last_name', 'premium', 'created_at', 'auth_type', 'subtopics')
+        fields = ('id', 'username', 'email', 'first_name', 'last_name', 'premium', 'created_at', 'auth_type', 'subforums')
 
 class CommentSerializer(serializers.ModelSerializer):
     class Meta:
@@ -30,4 +30,4 @@ class DiscussionSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Discussion
-        fields = ('id', 'title', 'description', 'upvote', 'downvote', 'owner', 'created_at', 'subtopic', 'comments', 'comment_count')
+        fields = ('id', 'title', 'description', 'upvote', 'downvote', 'owner', 'created_at', 'subforum', 'comments', 'comment_count')
