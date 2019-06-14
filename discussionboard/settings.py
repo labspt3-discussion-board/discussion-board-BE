9f880a830a209a41db51fdaa6f56184133c1be41
Brandon <brandon.g.elzy@gmail.com>
2019-06-14 16:38:41 -0500

    Test

--- discussionboard/settings.py
+++ discussionboard/settings.py
@@ -48,8 +48,10 @@ CORS_ALLOW_HEADERS = list(default_headers) + [
 
 CORS_ALLOW_CREDENTIALS = True
 SESSION_COOKIE_SAMESITE = None
+CSRF_COOKIE_SAMESITE = None
 # SESSION_COOKIE_DOMAIN = 'lambda-discussion-board-test.herokuapp.com/'
 SESSION_COOKIE_HTTPONLY = False
+
 
 # Rest Framework global settings
 REST_FRAMEWORK = {
