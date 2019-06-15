199f17bfcc5e0c34a5fc63703a0bbdefc933d36f
Xavier Puentes <xavier@Xaviers-MacBook-Pro.local>
2019-06-15 15:46:24 -0500

    Added default pagination of 50

--- discussionboard/settings.py
+++ discussionboard/settings.py
@@ -57,8 +57,10 @@ REST_FRAMEWORK = {
         'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly',
         'rest_framework.authentication.BasicAuthentication',
         'rest_framework.authentication.SessionAuthentication',
-        
+
     ],
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
+    'PAGE_SIZE': 50,
     # 'DEFAULT_AUTHENTICATION_CLASSES': (
     #     'oauth2_provider.contrib.rest_framework.OAuth2Authentication',
     #     'rest_framework_social_oauth2.authentication.SocialAuthentication',
